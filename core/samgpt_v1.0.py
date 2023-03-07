79d27a4b9c10581ac49df72a5d3c736e3e11f353
Sambiase <marsamb@gmail.com>
2023-03-07 19:40:29 -0300

    new files added

--- core/samgpt_v1.0.py
+++ core/samgpt_v1.0.py
@@ -1,5 +1,24 @@
 from decouple import config
+import openai
 
 SECRET_KEY = config('SECRET_KEY')
+openai.api_key = SECRET_KEY
 
+messages = [
+    {"role": "system",
+     "content": "You're a recruiter who asks interview questions. You ask one new question after my response"}
+]
 
+while True:
+    print('*'*23)
+    content = input("Please write something: ")
+    messages.append({"role": "user", "content": content})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    messages.append({"role": "assistant", "content": chat_response})
